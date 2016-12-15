70767591764e3d8c8c8e11f5e04100865e830767
kof.kofktu <kof.kofktu@kakaocorp.com>
2016-12-15 16:24:09 +0900

    프로젝트 초기셋팅

---
+++ KUIDragStepperSlider/KUIDragStepperSlider/KUIDragStepperSlider.h
@@ -0,0 +1,19 @@
+//
+//  KUIDragStepperSlider.h
+//  KUIDragStepperSlider
+//
+//  Created by kofktu on 2016. 12. 15..
+//  Copyright © 2016년 Kofktu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KUIDragStepperSlider.
+FOUNDATION_EXPORT double KUIDragStepperSliderVersionNumber;
+
+//! Project version string for KUIDragStepperSlider.
+FOUNDATION_EXPORT const unsigned char KUIDragStepperSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KUIDragStepperSlider/PublicHeader.h>
+
+
